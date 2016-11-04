a0516ba93fc72d01e8891e539e41561b0efcd4e8
Kdaryanto <aryahakage@gmail.com>
2016-11-04 11:14:28 -0700

    12141372 Membuat Fungsi x sebagai variabel inputan panjang / jumlah karakter

--- src/TugasSegitiga.java
+++ src/TugasSegitiga.java
@@ -3,3 +3,13 @@
  * NIM  : 12141372
  */
 import java.util.Scanner;
+
+public class TugasSegitiga {
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner (System.in);
+        System.out.println("Masukkan Jumlah Karakter : ");
+        int x = input.nextInt();
+    }
+    
+}
