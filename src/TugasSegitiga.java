b7bd5a48cef4434f367eec0bdd81ea90ba06f8c3
Kdaryanto <aryahakage@gmail.com>
2016-11-04 11:20:22 -0700

    12141372 Membuat Segitiga siku-siku kanan bawah. membuat perulangan untuk membentuk karakter * (bintang) tercetak secara berurutan

--- src/TugasSegitiga.java
+++ src/TugasSegitiga.java
@@ -24,6 +24,9 @@ public class TugasSegitiga {
             for(int j=0;j<=x - i;j++){
                 System.out.print(" ");
             }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
             System.out.println(" ");
         }
        
