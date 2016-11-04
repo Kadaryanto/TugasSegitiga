dd2fd3996c5577eecbd2faf9224bb01b29942514
Kdaryanto <aryahakage@gmail.com>
2016-11-04 11:32:09 -0700

    12141372 Membuat Segitiga Piramida Sama kaki, melakuka perulangan untuk mencetak spasi (kosong)dan membuat karakter * pada bagian pembentuk piramida

--- src/TugasSegitiga.java
+++ src/TugasSegitiga.java
@@ -36,7 +36,12 @@ public class TugasSegitiga {
             for(int j=x;j>=i;j--){
                 System.out.print(" ");
             }
-            
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            for (int l=1;l<=i-1;l++){
+                System.out.print("*");
+            }
             System.out.println();
         }
         
